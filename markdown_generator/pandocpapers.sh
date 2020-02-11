11faed854912f9d81724afc1cfbf852f5c5badff
MatisPatel <matis.patel@gmail.com>
2020-02-11 13:12:41 +0000

    adding publication on spite

--- markdown_generator/pandocpapers.sh
+++ markdown_generator/pandocpapers.sh
@@ -1,1 +1,1 @@
-pandoc --filter pandoc-citeproc --natbib --bibliography=MyPapers.bib --csl=nature.csl -s test.md -o ../_pages/publications.html
+pandoc --filter pandoc-citeproc --natbib --bibliography=MyPapers.bib --csl=nature.csl -s test.tex -o ../_pages/publications.html
